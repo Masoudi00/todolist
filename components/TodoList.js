3925da23f343b5bd75f99ef662eca20e1e70d7fe
Masoudi00 <139173182+Masoudi00@users.noreply.github.com>
2023-10-27 10:24:31 +0100

    first commit

---
+++ components/TodoList.js
@@ -0,0 +1,55 @@
+// To-do List 
+// list
+// input
+// button
+//onclick take input value and add it to the list
+//auto empty input
+//const[StateVariable, Function] = useState([]);
+
+import { useState } from "react";
+//TASK STATE
+const TodoList = () => {
+    const[tasks, setTasks] = useState([]);
+    const[taskText, setTaskText] = useState('');
+    
+
+
+//ADD TASK
+
+const AddTask = () => {
+    if(taskText) {
+        setTasks([...tasks, taskText]);
+        }
+        setTaskText('');
+}
+//remove Task 
+const removeTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+  return(
+    <div>
+    <h2>To-Do List</h2>
+    <ul>
+      {tasks.map((task, index) => (
+        <li key={index}>
+          {task} <button onClick={() => removeTask(index)}>Remove</button>
+        </li>
+      ))}
+    </ul>
+    <div>
+      <input
+        type="text"
+        value={taskText}
+        onChange={(e) => setTaskText(e.target.value)}
+      />
+      <button onClick={AddTask}>Add Task</button>
+    </div>
+  </div>
+
+  );
+};
+
+export default TodoList;
+
+
