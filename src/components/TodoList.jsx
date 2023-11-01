f3eeaa1e724d20c93aa294312457962bac141617
Masoudi00 <139173182+Masoudi00@users.noreply.github.com>
2023-11-01 11:18:57 +0100

    first commit

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const TodoList = () => {
+    const [tasks, setTasks] = useState([]);
+    const [taskText, setTaskText] = useState('');
+    const [doneTasks, setDoneTasks] = useState([]);
+
+    const AddTask = () => {
+        if (taskText) {
+            setTasks([...tasks, taskText]);
+            setTaskText('');
+        }
+    }
+
+    const markTaskAsDone = (index) => {
+        const taskToMarkAsDone = tasks[index];
+        setDoneTasks([...doneTasks, taskToMarkAsDone]);
+        removeTask(index);
+    }
+
+    const removeTask = (index) => {
+        const updatedTasks = tasks.filter((_, i) => i !== index);
+        setTasks(updatedTasks);
+    };
+
+    return (
+        <>
+            <div>
+                <h1>------Add Tasks--------</h1>
+                <div>
+                    <input
+                        className="form-control-3"
+                        id="exampleInputEmail1"
+                        type="text"
+                        value={taskText}
+                        onChange={(e) => setTaskText(e.target.value)}
+                    />
+                    <br /><br />
+                    <button className="btn btn-primary" onClick={AddTask}>Add Task</button>
+                </div>
+
+                <h1>--------Tasks-----------</h1>
+                <ul>
+                    {tasks.map((task, index) => (
+                      <>
+                        <li key={index}>
+                            {task} <button className="btn btn-danger btn-sm" onClick={() => removeTask(index)}>Remove</button>
+                            <button className="btn btn-success  btn-sm m-1" onClick={() => markTaskAsDone(index)}>Done</button>
+                        </li>
+                        <br />
+                        </>
+                    ))}
+                </ul>
+
+                <h1>--------Done Tasks-----------</h1>
+                <ul>
+                    {doneTasks.map((task, index) => (
+                        <li key={index}>
+                            {task}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    );
+};
+
+export default TodoList;
